7b22c2e1799a48552fd4f3dfd9e9d1740536d034
okkostenko <oleksandrkostenko0708@gmail.com>
2023-05-07 23:04:06 +0300

    added bollinger bends

--- statistics_calculation.py
+++ statistics_calculation.py
@@ -1,8 +1,14 @@
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
 
 
-def calc_ma(df:pd.DataFrame)->pd.DataFrame:
+# Volatility
+def calc_volatility(df:DataFrame) -> DataFrame:
+    pass
+
+# Moving Averege
+def calc_ma(df:DataFrame)->DataFrame:
     
     """Calculate moving averages for the last 50 and 200 datapoints."""
 
@@ -10,3 +16,29 @@ def calc_ma(df:pd.DataFrame)->pd.DataFrame:
     df["MA200"] = df["Close"].rolling(200).mean()
 
     return df
+
+# RSI
+def calc_rsi(df:DataFrame) -> DataFrame:
+    pass
+
+# Bollinger Bends
+def calc_bollinger_bends(df:DataFrame) -> DataFrame:
+    df["STD"] = df["Close"].rolling(20).std()
+
+    df["MB"] = df["Close"].rolling(20).mean()
+    df["UB"] = df["MB"] + 2 * df["STD"]
+    df["LB"] = df["MB"] - 2 * df["STD"]
+
+    return df
+
+# Fibonacci retracement
+def calc_fibonacci_retracement(df:DataFrame) -> DataFrame:
+    pass
+
+# OBV
+def calc_obv(df:DataFrame) -> DataFrame:
+    pass
+
+#MACD
+def calc_macd(df:DataFrame) -> DataFrame:
+    pass
