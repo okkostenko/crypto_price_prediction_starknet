76acd93d1cb796f96afaeb90a950ff5ce8cbc330
okkostenko <oleksandrkostenko0708@gmail.com>
2023-05-27 12:42:42 +0300

    model training constants

---
+++ model_training/constants.py
@@ -0,0 +1,11 @@
+NUMBER_OF_CLASSES = 6
+EPOCHS = 50
+BATCH_SIZE = 36
+
+TRAINING_PATH = "../data_collection/datasets/training"
+TEST_PATH = "../data_collection/datasets/test"
+
+VALIDATION_SPLIT = .1
+
+LEARNING_RATE = 0.005
+EPSILON = 3e-04
