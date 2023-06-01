adeb97796a00b9eea3d27801773300279a2f783b
okkostenko <oleksandrkostenko0708@gmail.com>
2023-06-02 01:04:29 +0300

    models training

--- model_training/constants.py
+++ model_training/constants.py
@@ -1,13 +1,14 @@
 NUMBER_OF_CLASSES = 5
 EPOCHS = 50
-BATCH_SIZE = 36
+BATCH_SIZE = 18
 
 TRAINING_PATH = "../data_collection/datasets/training"
 TEST_PATH = "../data_collection/datasets/test"
+FULL_PATH = "../data_collection/datasets/full/full"
 
 VALIDATION_SPLIT = .1
 
-LEARNING_RATE = 0.005
+LEARNING_RATE = 0.001
 EPSILON = 3e-04
 
 MODEL_LSTM_CLASS_OUTPUT_DIR = "../models/lstm/class"
